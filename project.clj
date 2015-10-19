13b404d87c39aa7200a044402d2c3b59eede5fab
Arthur Edelstein <arthuredelstein@gmail.com>
2015-10-19 01:35:56 -0400

    fix license, urls

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject torpatches "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Generate torpat.ch redirects and link pages."
+  :url "https://torpat.ch"
+  :license {:name "Public Domain"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [hiccup "1.0.5"]])
