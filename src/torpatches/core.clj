46ef6e559686049bb4bef580e4e10dba15d87be0
Arthur Edelstein <arthuredelstein@gmail.com>
2016-03-28 16:20:11 -0400

    make an isolation page

--- src/torpatches/core.clj
+++ src/torpatches/core.clj
@@ -36,7 +36,7 @@
   (->> (branches "../tor-browser")
        (filter #(.startsWith % "remotes/origin/tor-browser-"))
        sort
-       (sort-by #(match #"esr\-([^x]*+)" %))
+       ;(sort-by #(match #"esr\-([^x]*+)" %))
        last))
 
 (defn latest-commits
@@ -53,7 +53,8 @@
   [commit-message]
   (or (match #"(TB\d+)" commit-message)
       (match #"[Bb]ug \#?([0-9]+)" commit-message)
-      (match #"\#?([0-9]+)" commit-message)))
+      (match #"\#?([0-9]+)" commit-message)
+      "None"))
 
 (defn patch-url
   "Returns a URL for a tor-browser patch, given the hash."
@@ -63,17 +64,18 @@
 (defn remove-mozilla-commits
   "Remove mozilla commits, which are obvious from an 'r=' tag."
   [commits]
-  (remove #(let [[hash msg] %] (.contains msg "r=")) commits))
+  (remove #(let [[hash msg] %]
+             (or (.contains msg "r=")
+                 (.contains msg "a=")
+                 (.contains msg "No bug,")))
+          commits))
 
-(defn read-bugs-map
-  "Retrieve a map of trac.torproject.org ticket numbers to
-   a vector of [commit-hash commit-message]."
+(defn read-bugs-list
+  "Retrieve a list of [commit-hash commit-message]."
   [branch]
-  (->
-   (->> (latest-commits branch 200)
-        remove-mozilla-commits
-        (group-by #(-> % second bug-number)))
-   (dissoc nil)))
+  (->> (latest-commits branch 200)
+       remove-mozilla-commits
+       (remove nil?)))
 
 (defn separate
   "Returns [coll-true coll-false], where coll-true is every
@@ -84,11 +86,13 @@
    (remove pred coll)])
 
 (defn singles-and-multiples
-  "Takes the bug map and returns a vector pair of two bugs maps--
+  "Takes the bug list and returns a vector pair of two bugs maps--
    the first has bugs with single patches, the second
    bugs with multiple patches."
-  [bug-map]
-  (separate #(let [[k v] %] (= 1 (count v))) bug-map))
+  [bug-list]
+  (->> bug-list
+       (group-by #(-> % second bug-number))
+       (separate #(let [[k v] %] (= 1 (count v))))))
 
 (defn patch-url
   "Generates a Tor Browser patch url from the patch hash."
@@ -101,6 +105,13 @@
   [[trac-ticket [[hash _]]]]
   (str "location /" trac-ticket " { rewrite ^ " (patch-url hash) "; }\n"))
 
+(defn footer
+  "A footer for each page."
+  []
+  [:p [:span {:style "font-style: italic"} "Last update: " (now-string) " "]
+   [:span [:a {:href "https://github.com/arthuredelstein/torpatches"}
+           "(Source on github)"]]])
+
 (defn write-redirect-file
   "Create a redirect file from the single-patch bugs map."
   [single-patch-bugs]
@@ -108,21 +119,37 @@
    "/etc/nginx/redirects.txt"
    (apply str (map redirect-line single-patch-bugs))))
 
+(defn write-patch-list
+  "Create an HTML page that displays a list of links to patches
+   given in commits."
+  [tag title commits]
+  (spit
+   (str "../../torpat.ch/" tag)
+   (page/html5
+    [:head [:title title] [:meta {:charset "utf-8"}]]
+    [:body
+     [:h3 title]
+     [:pre
+      [:ul
+       (for [[hash message] commits]
+         [:li hash " " [:a {:href (patch-url hash)} message]])]]
+     (footer)])))
+
 (defn write-indirect-page
   "Create an HTML page that displays a list of links to patches
    for a given Tor Browser bug."
   [[ticket commits]]
-  (spit
-   (str "../../torpat.ch/" ticket)
-   (let [title (str "Patches for Tor Browser Bug #" ticket)]
-     (page/html5
-      [:head [:title title] [:meta {:charset "utf-8"}]]
-      [:body
-       [:h3 title]
-       [:pre
-        [:ul
-         (for [[hash message] commits]
-           [:li hash " " [:a {:href (patch-url hash)} message]])]]]))))
+  (let [title (str "Patches for Tor Browser Bug #" ticket)]
+    (write-patch-list ticket title commits)))
+
+(defn write-isolation-page
+  "Create an HTML page that displays a list of the isolation
+   pages."
+  [bugs-list]
+  (let [isolation-commits (filter #(-> % second (.contains "solat")) bugs-list)]
+    (write-patch-list "isolation"
+                      "Tor Browser Isolation Patches"
+                      isolation-commits)))
 
 (defn now-string
   "Returns the current date-time in UTC as a reasonably readable string."
@@ -147,10 +174,8 @@
         [:a {:href (str "https://gitweb.torproject.org/tor-browser.git/log/?h="
                         branch)} branch]]
        [:li [:a {:href "https://bugzilla.mozilla.org/buglist.cgi?quicksearch=whiteboard%3A[tor]"}
-                "whiteboard:[tor] bugs on bugzilla.mozilla.org"]]]]
-     [:p [:span {:style "font-style: italic"} "Last update: " (now-string) " "]
-         [:span [:a {:href "https://github.com/arthuredelstein/torpatches"}
-                    "(Source on github)"]]]
+             "whiteboard:[tor] bugs on bugzilla.mozilla.org"]]]]
+     (footer)
      ])))
 
 (defn -main [& args]
@@ -160,10 +185,12 @@
    https://gitweb.torproject.org. For bugs with multiple patches,
    creates a page at https://torpat.ch/#### that links to each of those patches."
   (let [branch (newest-tor-browser-branch)
-        bugs-map (read-bugs-map branch)
-        [single-patch-bugs multi-patch-bugs] (singles-and-multiples bugs-map)]
+        bugs-list (read-bugs-list branch)
+        [single-patch-bugs multi-patch-bugs] (singles-and-multiples bugs-list)]
     (write-redirect-file single-patch-bugs)
     (println "Wrote redirects file.")
+    (write-isolation-page bugs-list)
+    (println "Wrote isolation page.")
     (dorun (map write-indirect-page multi-patch-bugs))
     (println "Wrote multipatch link files.")
     (write-index (last (.split branch "/")))
